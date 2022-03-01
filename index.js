6fe4ebc21a5fb54a129ceb93bbdae0997ab17890
Areejellay02 <areejellay@gmail.com>
2022-03-01 10:07:26 +0000

    3/1/2022 routing de page

--- index.js
+++ index.js
@@ -20,9 +20,38 @@ app.get('/', function (req, res) {
     layout: 'main'
   });
 });
-
+app.get('/acceuil', function (req, res) {
+  res.render("acceuil", {
+    layout: 'main'
+  });
+});
 app.get('/login', function (req, res) {
   res.render("login", {
+    layout: 'main'
+  });
+});
+app.get('/profile', function (req, res) {
+  res.render("profile", {
+    layout: 'main'
+  });
+});
+app.get('/add', function (req, res) {
+  res.render("add", {
+    layout: 'main'
+  });
+});
+app.get('/product', function (req, res) {
+  res.render("product", {
+    layout: 'main'
+  });
+});
+app.get('/explore', function (req, res) {
+  res.render("explore", {
+    layout: 'main'
+  });
+});
+app.get('/header', function (req, res) {
+  res.render("header", {
     layout: 'main'
   });
 });
@@ -31,4 +60,15 @@ app.get('/footer', function (req, res) {
     layout: 'main'
   });
 });
+app.get('/autrecomm', function (req, res) {
+  res.render("autrecomm", {
+    layout: 'main'
+  });
+});
+app.get('/mescomm', function (req, res) {
+  res.render("mescomm", {
+    layout: 'main'
+  });
+});
+
 app.listen(3000);
