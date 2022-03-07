ca77531ad78a17ad08bc9302844d77d777e66dac
Areejellay02 <areejellay@gmail.com>
2022-03-07 17:18:23 +0000

    backend de login et search bar

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const app = express();
 const path = require('path');
+const mysql = require('mysql');
+const session = require('express-session');
+const { Router } = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+var createError = require('http-errors');
 
-var mysql = require('mysql');
-
-var pool = mysql.createConnection({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nassim",
-  port: '3200'
+const pool = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nassim'
 });
 
 pool.connect(function (err) {
   if (err) throw err;
 
 });
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use(express.static('public'));
 
@@ -31,13 +41,70 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 // Routes
-
 app.get('/', function (req, res) {
   res.render("index", {
     layout: 'main'
   });
 });
+/*
+// http://localhost:3000/
+app.get('/login', function(request, response) {
+	// Render login template
+  response.sendFile(path.join(__dirname + '/views/login.hbs'));
+});
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM utilisateur WHERE nom = ? AND mot_de_passe = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
 
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+//search bar 
+app.get('/explore', function(req, res) {
+  pool.query('SELECT nom FROM produit WHERE nom LIKE "%' + req.query.term + '%"',
+  function(err, rows, fields) {
+  if (err) throw err;
+  var data = [];
+  for (i = 0; i < rows.length; i++) {
+  data.push(rows[i].nom);
+  }
+  res.end(JSON.stringify(data));
+  });
+  });*/
 
 app.get('/login', function (req, res) {
   res.render("login", {
@@ -91,5 +158,4 @@ app.get('/mescommande', function (req, res) {
     layout: 'main'
   });
 });
-
-app.listen(3200);
+app.listen(3000);
